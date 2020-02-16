9d1ad84edc39dc56fd07f0b682f40aab6b4ebbb0
Knight H <thanapapash15@rism.ac.th>
2020-02-16 15:00:16 +0000

    user active deactive

--- src/js/components/user-managment/user-managment.js
+++ src/js/components/user-managment/user-managment.js
@@ -12,9 +12,25 @@ import { API_URL_DATABASE } from '../../config_database.js';
 
 class componentUserManagement extends React.Component {
 
-    state = {
-        allUsers: [],
-        isTokenValid: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            allUsers: [],
+            isActive: true,
+            isTokenValid: false
+        }
+        this.handleChangeStatus = this.handleChangeStatus.bind(this);
+    }
+
+    handleChangeStatus(event) {
+        // console.log(event.target.value);
+        if(event.target.value === "1"){
+            this.setState({ isActive: true,  allUsers: this.state.allUsers})
+        }
+        else{
+            this.setState({ isActive: false,  allUsers: this.state.allUsers })
+        }
+        
     }
 
     componentDidMount() {
@@ -23,7 +39,7 @@ class componentUserManagement extends React.Component {
         var current = this;
         axios.get(`http://vanilla-erp.com:${API_URL_DATABASE}/api/v1/users`, { headers: { 'x-access-token': token_auth } })
             .then(res => {
-                console.log(res)
+                // console.log(res.data)
                 const allUsers = res.data;
                 // console.log(persons.length)
                 this.setState({ allUsers: allUsers, isTokenValid: true });
@@ -43,11 +59,11 @@ class componentUserManagement extends React.Component {
     }
 
     nameUpperLower(data) {
-        console.log(data);
+        // console.log(data);
         if (data === null || data === "") {
             return data
         }
-        else{
+        else {
             var count = data.length;
             var firstChar = data.substring(0, 1).toUpperCase();
             var anotherChar = data.substring(1, count).toLowerCase();
@@ -58,47 +74,88 @@ class componentUserManagement extends React.Component {
     }
 
     render() {
-        return (
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Username</th>
-                        <th>Role</th>
-                        <th>Create At</th>
-                        <th>Last login</th>
-                        <th>Status</th>
-                        <th>Update</th>
-                    </tr>
-                </thead>
+        var current = this;
+        console.log("state", this.state.isActive)
+        console.log("state", this.state.allUsers)
 
-                <tbody>
-                    {this.state.allUsers.map((allUser) => (
+        return (
+            <>
+                <select className="form-control" onChange={this.handleChangeStatus}>
+                    <option value="1" selected>Active</option>
+                    <option value="0">Deactive</option>
+                </select>
+                <table className="table">
+                    <thead>
                         <tr>
-                            <td style={{ width: '5px' }}>
-                                <img src={logo} className="rounded-circle" style={{ width: '42px' }} alt="ture" />
-                            </td>
-                            <td>
-                                {this.nameUpperLower(allUser.firstname)} {this.nameUpperLower(allUser.lastname)}
-                            </td>
-                            <td>
-                                {allUser.username}
-                            </td>
-                            <td>{allUser.name}</td>
-                            <td>01 Oct 2019 13.00</td>
-                            <td>31 Oct 2019 22.00</td>
-                            <td> {this.checkActive(allUser.active)} </td>
-                            <td>
-                                <form action="_users_profile_admin.html">
-                                    <Link to={`/admin-users-profile/${allUser.username}`} ><button type="submit" className="btn btn-warning">แก้ไขข้อมูล</button></Link>
-                                </form>
-                            </td>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Username</th>
+                            <th>Role</th>
+                            <th>Create At</th>
+                            <th>Last login</th>
+                            <th>Status</th>
+                            <th>Update</th>
                         </tr>
-                    ))}
-                    <div className="mb-5"></div>
-                </tbody>
-            </table>
+                    </thead>
+
+                    <tbody>
+                        {this.state.allUsers.map(function (allUser) {
+                            if (current.state.isActive === true && allUser.active === 1) {
+                                console.log("active",allUser.username)
+                                return (
+                                    <tr>
+                                        <td style={{ width: '5px' }}>
+                                            <img src={logo} className="rounded-circle" style={{ width: '42px' }} alt="ture" />
+                                        </td>
+                                        <td>
+                                            {current.nameUpperLower(allUser.firstname)} {current.nameUpperLower(allUser.lastname)}
+                                        </td>
+                                        <td>
+                                            {allUser.username}
+                                        </td>
+                                        <td>{allUser.name}</td>
+                                        <td>01 Oct 2019 13.00</td>
+                                        <td>31 Oct 2019 22.00</td>
+                                        <td> {current.checkActive(allUser.active)} </td>
+                                        <td>
+                                            <form action="_users_profile_admin.html">
+                                                <Link to={`/admin-users-profile/${allUser.username}`} ><button type="submit" className="btn btn-warning">แก้ไขข้อมูล</button></Link>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                            if (current.state.isActive === false && allUser.active === 0) {
+                                console.log("deactive",allUser.username)
+
+                                return (
+                                    <tr>
+                                        <td style={{ width: '5px' }}>
+                                            <img src={logo} className="rounded-circle" style={{ width: '42px' }} alt="ture" />
+                                        </td>
+                                        <td>
+                                            {current.nameUpperLower(allUser.firstname)} {current.nameUpperLower(allUser.lastname)}
+                                        </td>
+                                        <td>
+                                            {allUser.username}
+                                        </td>
+                                        <td>{allUser.name}</td>
+                                        <td>01 Oct 2019 13.00</td>
+                                        <td>31 Oct 2019 22.00</td>
+                                        <td> {current.checkActive(allUser.active)} </td>
+                                        <td>
+                                            <form action="_users_profile_admin.html">
+                                                <Link to={`/admin-users-profile/${allUser.username}`} ><button type="submit" className="btn btn-warning">แก้ไขข้อมูล</button></Link>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        })}
+                        <div className="mb-5"></div>
+                    </tbody>
+                </table>
+            </>
 
         )
     };
